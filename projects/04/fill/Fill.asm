ee9338f14dc17d1f5e0571aa1651ca4ea059958a
James Stuber <jestuber@gmail.com>
2017-08-02 08:46:58 -0700

    Multiplication and fill the screen on keypress. In hack assembly.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,5 +10,75 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+// screen is 256x512 pixels
 
 // Put your code here.
+	// initialze current state variable
+	@ONOFF
+	M = 0
+
+(START)
+	@KBD
+	D = M // D = current key press
+// Listen for non-zero keyboard code
+	@BLACK
+	D;JNE // if D != 0 goto BLACK
+	@WHITE
+	0;JMP // goto WHITE if no key is pressed
+
+(BLACK)
+	
+	@ONOFF // check if already black
+	D = M
+	@START
+	D;JLT // if @ONOFF is negative it's already black, no need to draw again
+
+	@i
+	M = 0			// i = 0
+	@SCREEN
+	D = A
+	@address
+	M = D			// address = 16384 (base address of the Hack screen)
+	@ONOFF
+	M = -1 // set ONOFF to black
+	@LOOP
+	0;JMP // goto LOOP
+
+(WHITE)
+
+	@ONOFF // check if already white
+	D = M
+	@START
+	D;JEQ // if @ONOFF is 0 it's already black, no need to draw again
+
+	@i
+	M = 0			// i = 0
+	@SCREEN
+	D = A
+	@address
+	M = D			// address = 16384 (base address of the Hack screen)
+	@ONOFF
+	M = 0 // set ONOFF to white
+	@LOOP
+	0;JMP // goto LOOP
+
+(LOOP)
+   @i
+   D = M
+   @8191
+   D = D - A
+   @START
+   D;JGT		// if i > n goto START
+
+   @ONOFF
+   D = M
+   @address
+   A = M		// writing to memory using a pointer
+   M = D		// -1 for black, 0 for white
+ 
+   @i
+   M = M + 1	// i = i + 1
+   @address
+   M = M + 1	// address = address + 1
+   @LOOP
+   0;JMP		// goto LOOP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// while (R1 >= 0) add R0 to R2, R1--
+	@R2
+	M=0 // R2=0
+(LOOP)
+	@R1
+	D=M // D=R1
+	@END
+	D;JLE // if R1<=0 goto end
+	@R0
+	D=M // D=R0
+	@R2
+	M=D+M // R2=R0+R2
+	@R1
+	M=M-1 // i--
+	@LOOP
+	0;JMP
+
+// End program with endless loop
+(END)
+	@END
+	0;JMP
